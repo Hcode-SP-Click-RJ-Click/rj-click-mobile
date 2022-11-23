d9f791950fee808dd9e2c009222ceae3efe67f95
Joao Rangel <joaohcrangel@gmail.com>
2022-11-22 23:04:12 -0300

    aula 2

--- App.tsx
+++ App.tsx
@@ -1,57 +1,54 @@
-import styled from '@emotion/native';
-import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { Screen, Screens } from './src/routes';
+import {
+  FavoritesScreen,
+  HomeScreen,
+  ProfileScreen,
+  TripScreen,
+} from './src/screens';
+import { Colors } from './src/values/colors';
 
-const Container = styled.View`
-  flex: 1;
-  background-color: #fff;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Title = styled.Text`
-  font-size: 25px;
-  font-weight: bold;
-  text-transform: uppercase;
-`;
-
-const PlaceContainer = styled.View`
-  border-color: blue;
-  border-width: 1px;
-  border-style: solid;
-  height: 200px,
-  width: 100%;
-`;
-
-const PlaceImage = styled.ImageBackground`
-  height: 200px;
-  justify-content: flex-end;
-  padding-left: 15px;
-  padding-bottom: 15px;
-`;
-
-const PlaceTitle = styled.Text`
-  font-size: 24px;
-  color: #fff;
-`;
-
-const PlaceSubTitle = styled.Text`
-  color: #fff;
-`;
+const Tab = createMaterialBottomTabNavigator<typeof Screens>();
 
 export default function App() {
   return (
-    <Container>
-      <Title>Descubra lugares incríveis!</Title>
-      <PlaceContainer>
-        <PlaceImage source={require('./assets/maracana.jpg')}>
-          <PlaceTitle>Maracanã</PlaceTitle>
-          <PlaceSubTitle>Um dos estádios mais famosos do mundo.</PlaceSubTitle>
-        </PlaceImage>
-      </PlaceContainer>
-      <StatusBar style="auto" />
-    </Container>
+    <NavigationContainer>
+      <Tab.Navigator
+        barStyle={{ backgroundColor: Colors.theme }}
+        activeColor={Colors.primary}
+        inactiveColor={Colors.themeContrast}
+        screenOptions={{ title: '' }}
+      >
+        <Tab.Screen
+          name={Screen.Home}
+          component={HomeScreen}
+          options={{
+            tabBarIcon: 'home',
+          }}
+        />
+        <Tab.Screen
+          name={Screen.Trip}
+          component={TripScreen}
+          options={{
+            tabBarIcon: 'bag-suitcase',
+          }}
+        />
+        <Tab.Screen
+          name={Screen.Favorites}
+          component={FavoritesScreen}
+          options={{
+            tabBarIcon: 'cards-heart',
+          }}
+        />
+        <Tab.Screen
+          name={Screen.Profile}
+          component={ProfileScreen}
+          options={{
+            tabBarIcon: 'account',
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({});
---
+++ src/routes/index.ts
@@ -0,0 +1,13 @@
+export enum Screen {
+  Home = 'Home',
+  Trip = 'Trip',
+  Favorites = 'Favorites',
+  Profile = 'Profile',
+}
+
+export const Screens = {
+  [Screen.Home]: undefined,
+  [Screen.Trip]: undefined,
+  [Screen.Favorites]: undefined,
+  [Screen.Profile]: undefined,
+};
